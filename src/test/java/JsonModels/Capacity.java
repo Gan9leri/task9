5482a7f59a6e3c5f9a23322ec06c960631764963
AOvsyannikov <aovsyannikov@it-one.ru>
2024-05-17 18:03:22 +0300

    added Capacity.java

---
+++ src/test/java/JsonModels/Capacity.java
@@ -0,0 +1,32 @@
+package JsonModels;
+
+public class Capacity {
+
+    Integer countFreezer;
+    Integer countShelf;
+    Integer countDoor;
+
+    public Integer getCountFreezer() {
+        return countFreezer;
+    }
+
+    public void setCountFreezer(Integer countFreezer) {
+        this.countFreezer = countFreezer;
+    }
+
+    public Integer getCountShelf() {
+        return countShelf;
+    }
+
+    public void setCountShelf(Integer countShelf) {
+        this.countShelf = countShelf;
+    }
+
+    public Integer getCountDoor() {
+        return countDoor;
+    }
+
+    public void setCountDoor(Integer countDoor) {
+        this.countDoor = countDoor;
+    }
+}
