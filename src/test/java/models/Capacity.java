4e6a0074941fa6978dee515af3c447087596788d
AOvsyannikov <aovsyannikov@it-one.ru>
2024-05-19 13:56:01 +0300

    updated Capacity.java

--- src/test/java/models/Capacity.java
+++ src/test/java/models/Capacity.java
@@ -1,4 +1,4 @@
-package JsonModels;
+package models;
 
 public class Capacity {
 
@@ -10,23 +10,11 @@ public class Capacity {
         return countFreezer;
     }
 
-    public void setCountFreezer(Integer countFreezer) {
-        this.countFreezer = countFreezer;
-    }
-
     public Integer getCountShelf() {
         return countShelf;
     }
 
-    public void setCountShelf(Integer countShelf) {
-        this.countShelf = countShelf;
-    }
-
     public Integer getCountDoor() {
         return countDoor;
-    }
-
-    public void setCountDoor(Integer countDoor) {
-        this.countDoor = countDoor;
     }
 }
